aed0f605cf50a0139f2a45ef175ff239712a435d
Tanju ALKAN <74377224+talkan67@users.noreply.github.com>
2024-11-24 16:12:39 +0300

    Update k8sinstallation.sh

--- k8sinstallation.sh
+++ k8sinstallation.sh
@@ -26,3 +26,4 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+(worker nfs support): sudo apt-get install -y nfs-common
